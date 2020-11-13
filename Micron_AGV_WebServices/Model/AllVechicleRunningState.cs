0d9deef4ef3bff5d319ef0627a0850a9833a7660
JeremyPeng-NB <JeremyPeng@yuchens.com>
2020-11-13 18:50:36 +0800

    Test API 整合功能 [範例]
    Test API 整合功能 [範例]

---
+++ Micron_AGV_WebServices/Model/AllVechicleRunningState.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Micron_AGV_WebServices.Model
+{
+    //public class AllVechicleRunningState
+    //{
+    //    public VechicleRunningState[] result { get; set; }
+    //}
+
+    public class AllVechicleRunningState
+    {
+        public string agvName { get; set; }
+
+        public int entityId { get; set; }
+
+        public int speed { get; set; }
+
+        public int x { get; set; }
+
+        public int y { get; set; }
+
+        public int battery { get; set; }
+
+        //public string error { get; set; }
+
+        public int agvState { get; set; }
+
+        public int enabled { get; set; }
+    }
+}
